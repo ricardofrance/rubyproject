1ec5ce84cedc569821858695212a5c59524e397f
ricardofrance <ricardolfrance@gmail.com>
2013-02-11 14:25:03 -0200

    Instalação Esqueleto RoR
    Início dos estudos em Ruby on Rails. Exercício do Livro Desenvolvendo
    Aplicações Web com Ryby on Rails 2.3 e PostgreSQL. Autor Ricardo Roberto
    Leme.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rubyproject::Application.config.secret_token = 'd205e7582d42f4ce0e2ea3bd877eb7c96472e697363d8b6d3631601a8df2f838ccd1a84f532b1825d75c1918a53048f47e2fe6af0838c2932956002e01c54a3c'
